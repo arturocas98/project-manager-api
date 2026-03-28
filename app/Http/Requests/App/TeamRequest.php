d612509030e047cf716f1248c28ad257886d0761
arturocas98 <arturocastro98@hotmail.com>
2026-03-28 10:47:08 -0500

    Update TeamRequest.php

--- app/Http/Requests/App/TeamRequest.php
+++ app/Http/Requests/App/TeamRequest.php
@@ -14,9 +14,9 @@ class TeamRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => ['required', 'string', 'max:255'],
+            'name' => ['required', 'string', 'max:255', 'unique:teams,name'],
             'type' => ['nullable', 'string'],
-            'client_id' => ['nullable', 'integer', 'exists:clients,id'],
+            'client_id' => ['required', 'integer', 'exists:clients,id'],
         ];
     }
 }
