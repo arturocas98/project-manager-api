bf7232173a518d6da7e4d846037ef65b795c6193
arturocas98 <arturocastro98@hotmail.com>
2026-03-28 09:39:10 -0500

    update enums

--- lang/es/enums.php
+++ lang/es/enums.php
@@ -1,10 +1,31 @@
 <?php
 
+use App\Enums\Province;
 use App\Enums\UserModality;
 
 return [
     UserModality::class => [
         UserModality::Presentail->value => 'Presencial',
         UserModality::HomeOffice->value => 'Teletrabajo',
+    ],
+    Province::class => [
+        Province::AZUAY->value => 'Azuay',
+        Province::BOLIVAR->value => 'Bolívar',
+        Province::CAÑAR->value => 'Cañar',
+        Province::CARCHI->value => 'Carchi',
+        Province::CHIMBORAZO->value => 'Chimborazo',
+        Province::COTOPAXI->value => 'Cotopaxi',
+        Province::EL_ORO->value => 'El Oro',
+        Province::GUAYAS->value => 'Guayas',
+        Province::IMBABURA->value => 'Imbabura',
+        Province::LOJA->value => 'Loja',
+        Province::MANABI->value => 'Manabí',
+        Province::MORONA_SANTIAGO->value => 'Morona Santiago',
+        Province::NAPO->value => 'Napo',
+        Province::ORRELLANA->value => 'Orellana',
+        Province::PASTAZA->value => 'Pastaza',
+        Province::PICHINCHA->value => 'Pichincha',
+        Province::TUNGURAHUA->value => 'Tungurahua',
+        Province::ZAMORA_CHINCHIPE->value => 'Zamora Chinchipe',
     ],
 ];
--- routes/api/_app.php
+++ routes/api/_app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CantonController;
 use App\Http\Controllers\IncidenceAssignedController;
 use App\Http\Controllers\IncidenceComentController;
 use App\Http\Controllers\IncidenceController;
@@ -10,6 +11,7 @@ use App\Http\Controllers\ProjectMemberController;
 use App\Http\Controllers\NotificationController;
 use App\Http\Controllers\MediaController;
 use App\Http\Controllers\ClientController;
+use App\Http\Controllers\ProvinceController;
 use App\Http\Middleware\CheckRole;
 
 Route::get('projects/{project}/files', [ProjectController::class, 'files'])
@@ -60,7 +62,7 @@ Route::middleware(['auth:api', CheckRole::class])->group(function () {
         ->name('projects.incidences.coments.update');
     Route::delete('projects/{project}/incidences/{incidence}/comments/{comment}', [IncidenceComentController::class, 'destroy'])
         ->name('projects.incidences.coments.delete');
-        
+
     Route::get('projects/{project}/messages', [MessageController::class, 'index'])
         ->name('projects.messages.index');
     Route::post('projects/{project}/messages', [MessageController::class, 'store'])
@@ -94,4 +96,7 @@ Route::middleware(['auth:api', CheckRole::class])->group(function () {
         ->name('media.show')->whereNumber('media');
     Route::delete('media/{media}', [MediaController::class, 'destroy'])
         ->name('media.destroy')->whereNumber('media');
+
+    Route::get('provinces', [ProvinceController::class, 'index']);
+    Route::get('cantons', [CantonController::class, 'index']);
 });
